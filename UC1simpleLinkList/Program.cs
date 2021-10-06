90c66046a7f7d6efee693f57640147813835e940
Amit <amitkr24031995@gmail.com>
2021-10-06 17:44:39 +0530

    Adding UC1 simple Linked List

---
+++ UC1simpleLinkList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC1simpleLinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
+        }
+    }
+}
