735216ba4dd44ad4b9cf1ad0eb8b603d67664bf6
Amit <amitkr24031995@gmail.com>
2021-10-06 17:53:58 +0530

    Adding UC2createLinkedList

---
+++ UC1simpleLinkList/UC2LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2createLinkedList
+{
+    public class UC2LinkedList
+    {
+        internal UC2Node head;
+        internal void Add(int data)
+        {
+            UC2Node node = new UC2Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                UC2Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List ", node.data);
+        }
+        internal void Display()
+        {
+            UC2Node temp = this.head;
+            if (temp == null)
+            {
+                Console.Write("Linked List is empty : ");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
---
+++ UC1simpleLinkList/UC2Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC2createLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UC2LinkedList List = new UC2LinkedList();
+            List.Add(30 + 70);
+            List.Add(30);
+            List.Add(56 + 70);
+            List.Display();
+        }
+    }
+}
