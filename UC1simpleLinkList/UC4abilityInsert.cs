c123ba2c9d57d1886aef769c6bcfad40b90c084f
Amit <amitkr24031995@gmail.com>
2021-10-06 18:06:21 +0530

    Adding UC4abilityInsert

---
+++ UC1simpleLinkList/UC4abilityInsert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace UC4abilityInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList MyList = new ArrayList();
+            MyList.Add(56);
+            MyList.Add(70);
+            Console.Write("Before Inserting value: ");
+            foreach (object arr in MyList)
+            {
+                Console.Write(arr + " ");
+            }
+            Console.WriteLine();
+            MyList.Insert(1, 30);
+            Console.Write("After Inserting value: ");
+            foreach (object arr in MyList)
+            {
+                Console.Write(arr + " ");
+            }
+        }
+    }
+}
