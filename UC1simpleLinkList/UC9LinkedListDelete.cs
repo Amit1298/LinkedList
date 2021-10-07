1037e589e4ec6a883a02fe070bbf28f3ec520ee8
Amit <amitkr24031995@gmail.com>
2021-10-07 19:00:04 +0530

    Adding UC9LinkedListDelete

---
+++ UC1simpleLinkList/UC9LinkedListDelete.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC9LinkedListDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.AddLast(56);
+            myList.AddLast(30);
+            myList.AddLast(40);
+            myList.AddLast(70);
+            Console.WriteLine("Before Deleted element: ");
+            foreach (int num in myList)
+            {
+                Console.Write(num + " ");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------");
+            LinkedListNode<int> node = myList.Find(40);
+            myList.Remove(node);
+            Console.WriteLine("After Deleted element:");
+            foreach (int num in myList)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
