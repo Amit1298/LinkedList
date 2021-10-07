3eab7f1de8cffe79f752c574c404a0fb8bbfa6d8
Amit <amitkr24031995@gmail.com>
2021-10-07 18:51:06 +0530

    Added UC7LinkedList

---
+++ UC1simpleLinkList/UC7LinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC7LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.AddFirst(56);
+            myList.AddFirst(30);
+            myList.AddFirst(70);
+            LinkedListNode<int> node = myList.Find(30);
+            myList.AddAfter(node, 50);
+            myList.AddBefore(node, 40);
+            Console.WriteLine("Show the Number: ");
+            foreach (object obj in myList)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
